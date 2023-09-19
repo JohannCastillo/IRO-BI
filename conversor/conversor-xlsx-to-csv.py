3ee96fb2e1724b2ac7ba132d3e29774a43438938
JohannCastillo <johannjco15022@gmail.com>
2023-09-19 10:16:53 -0500

    add: output folder in conversor

--- conversor/conversor-xlsx-to-csv.py
+++ conversor/conversor-xlsx-to-csv.py
@@ -1,17 +1,17 @@
-# import pandas as pd
-
-# data_xls = pd.read_excel('excelfile.xlsx', 'Hoja1', index_col=None)
-# data_xls.to_csv('csvfile.csv', encoding='utf-8', index=False)
-
+import os
 import pandas as pd
 
-filePath = './Ubigeos/Provincias.xlsx'
+#Ruta del archivo a convertir 
+filePath = '../Ubigeos/Provincias.xlsx'
+
+
+output = os.path.join(os.getcwd(), 'conversor','data')
 
 xls = pd.ExcelFile(filePath)
 
 for sheet_name in xls.sheet_names:
     data_xls = pd.read_excel(filePath, sheet_name, index_col=None)
-    csv_filename = f'{sheet_name}.csv'
+    csv_filename = os.path.join(output, f'{sheet_name}.csv')
     data_xls.to_csv(csv_filename, encoding='utf-8', index=False)
 
 xls.close()
