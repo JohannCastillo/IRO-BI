cdfb46b07ae5bf67e8b51ec896b522651c463ce2
brunomont <brunoadrian.mont@gmail.com>
2023-09-21 06:03:39 -0500

    seed

--- faker/seed.ts
+++ faker/seed.ts
@@ -1,14 +1,13 @@
 import { PacienteSeeder } from './src/seeders/paciente.seeder'
 import { ContactoEmergenciaSeeder } from './src/seeders/contactoemergencia.seeder'
-//import { HistoriaSeeder } from './src/seeders/historia.seeder'
-
 import { AtencionSeeder } from "./src/seeders/atencion.seeder"
 import { CitaSeeder } from "./src/seeders/cita.seeder"
 
 function main() {
   PacienteSeeder.seed()
-  //HistoriaSeeder.seedCsv({ quantity: 50000, nAtenciones: 20 })
   ContactoEmergenciaSeeder.seed()
+  AtencionSeeder.seed()
+  CitaSeeder.seed()
 }
 
 main()
