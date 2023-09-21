c9da229de81b10ec82ab97f474c8534da061e80e
brunomont <brunoadrian.mont@gmail.com>
2023-09-21 06:02:32 -0500

    mod: add get sql consutal to get tipocita 2

--- faker/src/seeders/atencion.seeder.ts
+++ faker/src/seeders/atencion.seeder.ts
@@ -1,3 +1,4 @@
+import { CANTIDAD_ATENCIONES } from '../constants'
 import { formatCSV } from '../helpers/cvs'
 import { exportTo } from '../helpers/file'
 import { formatSqlOne } from '../helpers/sql'
@@ -6,17 +7,24 @@ interface SeedParams {
   quantity?: number | undefined
 }
 export class AtencionSeeder {
-  static seed({ quantity = 100 }: SeedParams = {}) {
+  static seed({ quantity = CANTIDAD_ATENCIONES }: SeedParams = {}) {
     let sentence = ''
     for (let i = 1; i <= quantity; i++) {
-      const atencion = new Atencion(i)
+      const atencion = new Atencion()
       const sql = formatSqlOne('ATENCION', atencion)
       sentence += sql + '\n'
     }
     exportTo('sql', sentence, 'atenciones')
   }
+  static getSqlConsulta({ idCita }: { idCita: number }) {
+    let sentence = ''
+    const atencion = new Atencion(null, idCita, 2)
+    const sql = formatSqlOne('ATENCION', atencion)
+    sentence += sql + '\n'
+    return sentence
+  }
 
-  static seedCsv({ quantity = 1000000 }: SeedParams = {}) {
+  static seedCsv({ quantity = CANTIDAD_ATENCIONES }: SeedParams = {}) {
     let sentence = ''
     for (let i = 1; i <= quantity; i++) {
       const atencion = new Atencion(i)
